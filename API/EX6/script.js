44200e165137bbce9e341a79eabc450af7c8ef7f
Permanent Records <andrew@permanentrecords.ca>
2020-02-24 16:44:54 -0800

    Last one!

--- API/EX6/script.js
+++ API/EX6/script.js
@@ -10,11 +10,11 @@ Else we show the following message:
 So, So, SOOO, sad.. the author is crying now... thanks
 */
 let authorName = window.confirm("Would you like the Author of this document?");
-let firstName = Andrew;
-let lastName = Nicholls;
+let firstName = "Andrew";
+let lastName = "Nicholls";
 if(authorName === true)
 {
-	alert("The author is %firstName% %lastName%");
+	alert("The author is " + firstName + " " + lastName);
 }
 else 
 {
